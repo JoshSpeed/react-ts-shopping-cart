edc105a1da829f257c24aa19d5134dac2506a5eb
Josh Speed <joshuabspeed@hotmail.co.uk>
2023-01-10 17:24:38 +0000

    Add context to shopping cart

--- src/components/StoreItem.tsx
+++ src/components/StoreItem.tsx
@@ -1,4 +1,6 @@
-import { Card } from 'react-bootstrap'
+import { Card, Button } from 'react-bootstrap'
+import { formatCurrency } from '../utilities/formatCurrency'
+import { useShoppingCart } from '../context/ShoppingCartContext'
 
 type StoreItemProps = {
   id: number
@@ -8,14 +10,50 @@ type StoreItemProps = {
 }
 
 export function StoreItem({ id, name, price, imgUrl }: StoreItemProps) {
+  const {
+    getItemQuantity,
+    increaseCartQuantity,
+    decreaseCartQuantity,
+    removeFromCart
+  } = useShoppingCart()
+  const quantity = getItemQuantity(id)
+
   return (
-    <Card>
+    <Card className='h-100'>
       <Card.Img
         variant='top'
         src={imgUrl}
         height='200px'
         style={{ objectFit: 'cover' }}
       />
+      <Card.Body className='d-flex flex-column'>
+        <Card.Title className='d-flex justify-content-between align-items-baseline mb-4'>
+          <span className='fs-2'>{name}</span>
+          <span className='ms-2 text-muted'>{formatCurrency(price)}</span>
+        </Card.Title>
+        <div className='mt-auto'>
+          {quantity === 0 ? (
+            <Button className='w-100'>+ Add To Cart</Button>
+          ) : (
+            <div
+              className='d-flex align-items-center flex-column'
+              style={{ gap: '.5rem' }}
+            >
+              <div
+                className='d-flex align-items-center justify-content-c'
+                style={{ gap: '.5rem' }}
+              >
+                <Button>-</Button>
+                <div>
+                  <span className='fs-3'>{quantity}</span> in cart
+                </div>
+                <Button>+</Button>
+              </div>
+              <Button variant='danger'>Remove</Button>
+            </div>
+          )}
+        </div>
+      </Card.Body>
     </Card>
   )
 }
---
+++ src/context/ShoppingCartContext.tsx
@@ -0,0 +1,84 @@
+import { createContext, ReactNode, useContext, useState } from 'react'
+
+// ReactNode is the type you give to children property in react TS
+type ShoppingCartProviderProps = {
+  children: ReactNode
+}
+
+type CartItem = {
+  id: number
+  quantity: number
+}
+
+type ShoppingCartContext = {
+  getItemQuantity: (id: number) => number
+  increaseCartQuantity: (id: number) => void
+  decreaseCartQuantity: (id: number) => void
+  removeFromCart: (id: number) => void
+}
+
+const ShoppingCartContext = createContext({} as ShoppingCartContext)
+
+export function useShoppingCart() {
+  return useContext(ShoppingCartContext)
+}
+
+export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
+  const [cartItems, setCartItems] = useState<CartItem[]>([])
+
+  function getItemQuantity(id: number) {
+    // ?. = if statement evaluates to something, then reutnr quantity, otherwise 0
+    return cartItems.find((item) => item.id === id)?.quantity || 0
+  }
+
+  function increaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id) == null) {
+        return [...currItems, { id, quantity: 1 }]
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity + 1 }
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  function decreaseCartQuantity(id: number) {
+    setCartItems((currItems) => {
+      if (currItems.find((item) => item.id === id)?.quantity === 1) {
+        return currItems.filter((item) => item.id !== id)
+      } else {
+        return currItems.map((item) => {
+          if (item.id === id) {
+            return { ...item, quantity: item.quantity - 1 }
+          } else {
+            return item
+          }
+        })
+      }
+    })
+  }
+
+  function removeFromCart(id: number) {
+    setCartItems((currItems) => {
+      return currItems.filter((item) => item.id !== id)
+    })
+  }
+
+  return (
+    <ShoppingCartContext.Provider
+      value={{
+        getItemQuantity,
+        increaseCartQuantity,
+        decreaseCartQuantity,
+        removeFromCart
+      }}
+    >
+      {children}
+    </ShoppingCartContext.Provider>
+  )
+}
