da2f8820c9d11a5d9f665c4da84eb9bc6ad1c702
Josh Speed <joshuabspeed@hotmail.co.uk>
2023-01-11 17:08:35 +0000

    finish project

--- src/context/ShoppingCartContext.tsx
+++ src/context/ShoppingCartContext.tsx
@@ -1,5 +1,6 @@
 import { createContext, ReactNode, useContext, useState } from 'react'
 import { ShoppingCart } from '../components/ShoppingCart'
+import { useLocalStorage } from '../hooks/useLocalStorage'
 
 // ReactNode is the type you give to children property in react TS
 type ShoppingCartProviderProps = {
@@ -30,7 +31,10 @@ export function useShoppingCart() {
 
 export function ShoppingCartProvider({ children }: ShoppingCartProviderProps) {
   const [isOpen, setIsOpen] = useState(false)
-  const [cartItems, setCartItems] = useState<CartItem[]>([])
+  const [cartItems, setCartItems] = useLocalStorage<CartItem[]>(
+    'shopping-cart',
+    []
+  )
 
   // Counts all item quantity for each item in cart and adds to cartQuantity
   const cartQuantity = cartItems.reduce(
